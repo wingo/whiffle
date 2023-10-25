70e346fd29ca62febbf2f57c8b93d444c6e180f6
Andy Wingo <wingo@igalia.com>
2023-10-25 11:35:00 +0200

    Fix linking against static libgc.a
    Thanks to Thorsten Ball; see https://github.com/wingo/whippet/pull/1.

--- build.mk
+++ build.mk
@@ -12,7 +12,7 @@ EMBEDDER_TO_GC_CFLAGS=$(WHIFFLE_CFLAGS) -include $(WHIFFLE)runtime/whiffle-gc.h
 out.o: out.c
 	$(GC_COMPILE) $(WHIFFLE_CFLAGS) $(GC_TO_EMBEDDER_CFLAGS) -c $<
 out: out.o $(GC_OBJS)
-	$(GC_LINK) $(GC_LIBS) $^
+	$(GC_LINK) $^ $(GC_LIBS)
 
 clean: 
 	$(GC_V)rm -f out out.o $(GC_OBJS)
